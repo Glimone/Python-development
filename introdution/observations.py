37c2b05ef9a64f5997215f6e5a29d22d778a4ea4
Gabriel <glimone.nunes@outlook.com>
2024-09-14 19:09:05 -0300

    new feature

---
+++ introdution/observations.py
@@ -0,0 +1,7 @@
+#Verifique o tipo do dado com a função type():
+x = 10
+print(type(x))
+palavra = "Hello World"
+print(type(palavra))
+
+#Se você usar CTRL + K e em seguida CTRL + C, vai comentar todas as linhas
