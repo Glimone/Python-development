af23c468ea247a15e5a9ba8ff4479402f88621bc
Gabriel <glimone.nunes@outlook.com>
2024-09-15 22:45:59 -0300

    Exercício Number impar ou par finalizado.

--- introdution/observations.py
+++ introdution/observations.py
@@ -1,7 +1,18 @@
-#Verifique o tipo do dado com a função type():
-x = 10
-print(type(x))
-palavra = "Hello World"
-print(type(palavra))
+# #Verifique o tipo do dado com a função type():
+# x = 10
+# print(type(x))
+# palavra = "Hello World"
+# print(type(palavra))
 
-#Se você usar CTRL + K e em seguida CTRL + C, vai comentar todas as linhas
+#Se você usar CTRL + K e em seguida CTRL + C, vai comentar todas as linhas 
+
+#Quando você quiser uma variável CONSTANTE, cuja nunca mudará no código, você deve escrever ela em maíuscula. 
+
+#ex: LOCAL_VIAGEM = "Recife"
+
+#você também pode ver o endereço de memória de um elemento em seu código, usando o id() nele.
+
+#Ex: 
+
+addresstest = 124 ** 2
+print(id(addresstest))
