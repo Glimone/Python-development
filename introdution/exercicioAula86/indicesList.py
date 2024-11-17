d1bfeed72e224992611e599936a041e2fadaa52c
Gabriel <glimone.nunes@outlook.com>
2024-11-17 12:07:38 -0300

    Melhora de código com enumerate

--- introdution/exercicioAula86/indicesList.py
+++ introdution/exercicioAula86/indicesList.py
@@ -1,6 +1,11 @@
-lista1 = [10, 20, 30, 40, 50, 60, 120, 44]
+lista1 = [10, 20, 30, 40, 50, 60, 120, 44, 48]
 
-indices = -1
-for x in lista1:
-    indices += 1
-    print(f"Indice {indices} do valor {x}")
+# indices = -1
+ 
+# for x in lista1:
+#     indices += 1
+#     print(f"Indice {indices} do valor {x}")
+
+for indice, x in enumerate(lista1):
+    print(f"Indice: {indice} numero: {x} ")
+
