7a8df94f4adab18d25495020794988faf11f8b75
Gabriel <glimone.nunes@outlook.com>
2024-11-19 00:26:24 -0300

    Explicando listas em listas

---
+++ introdution/listsIntroducao/listInList.py
@@ -0,0 +1,8 @@
+general = [
+    ["José", "Flávio", "Fernanda"], 
+    ["Francisco", "Cláudia"],
+    ["Rayan", "Gabriel", "Maria"]
+
+]
+
+print(general[1][1]) #Quando houver iteráveis dentro de outros, você pode acessá-los desse modo, colocando o indice do elemento no iterável pai, posteriormente o indice do subiterável dentro deste pai.
