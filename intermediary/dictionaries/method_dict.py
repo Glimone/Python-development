1d9c22d0b01691c5668498f3c3b30af943b9cb46
Gabriel <glimone.nunes@outlook.com>
2024-12-06 19:03:55 -0300

    Correção das pastas

--- intermediary/dictionaries/method_dict.py
+++ intermediary/dictionaries/method_dict.py
@@ -23,3 +23,28 @@ print(f"{carro.items()}\n")
 carro.setdefault('velocidade', 250) 
 print(carro['velocidade']) #Estrutura = dict_name.setdefault('keyName', value)
 
+#Get - Obtem chave do dicionário e retorna valor ou nome da chave.
+print(carro.get('Cor'))
+
+
+#pop - Serve para excluir uma chave, mas não necessariamente o valor. Ao usar o pop você pode transferir o valor para uma var. 
+
+ano = carro.pop('Ano')
+print(ano)
+print(f"{carro}\n") #Veja que o ano está na var agora e não aparece mais no dict.
+
+#popitem - Elima a última chave do dicionário. 
+
+ultima_chave = carro.popitem()
+print(ultima_chave)
+
+#update - Modo de atualizar totalmente o seu dict com a quantidade de chaves e valores que você desejar.
+
+carro.update( {
+
+    'ano': 2024,
+    'km': 0, 
+    'revestimento': 'prata',
+} )
+
+print(f"{carro}\n")
