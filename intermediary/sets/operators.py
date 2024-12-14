306bb520fa38bd9ac67b3a08b1d5f3ea532012b5
Gabriel <glimone.nunes@outlook.com>
2024-12-14 17:52:34 -0300

    Operators type for set

---
+++ intermediary/sets/operators.py
@@ -0,0 +1,13 @@
+s1 = {1,2,3}
+s2 = {2,3,4,5}
+
+#Union: | ou .union()
+print(s1.union(s2))
+#Intersection: & or .intersection()
+print(s1.intersection(s2))
+
+#Difference: - or .difference()
+print(s1.difference(s2))
+
+#Symetric difference: ^ or
+print(s1.symmetric_difference(s2))
